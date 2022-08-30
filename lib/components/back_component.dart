bf124b5225e30a947a69aa3bdebf0dae3ab797ef
Gurgen-Tumanyan <52563236+Gurgen-Tumanyan@users.noreply.github.com>
2022-08-30 17:09:25 +0400

    Update back_component.dart

--- lib/components/back_component.dart
+++ lib/components/back_component.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:morningmagic/resources/colors.dart';
-import 'package:morningmagic/widgets/primary_circle_button.dart';
 
 class BackComponent extends StatelessWidget {
+  const BackComponent({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topLeft,
-      child: PrimaryCircleButton(
-        icon: Icon(Icons.arrow_back, color: AppColors.primary),
-        onPressed: () => Get.back(),
-        size: 40,
+      child: GestureDetector(
+        child: const Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 4, 0),
+          child: Icon(Icons.west, color: Colors.white, size: 30),
+        ),
+        onTap: () => Get.back(),
       ),
     );
   }
